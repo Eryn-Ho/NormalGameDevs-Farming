fe6d865e2e9ec6c43dd9e9767d6197655d4c2e03
Ryan Ho <ryanho3d@outlook.com>
2024-10-01 02:08:33 -0400

    added tilling and watering requirements for planting seeds and growing plant

--- Assets/Scripts/RyanScripts/Plant_Growth_Script.cs
+++ Assets/Scripts/RyanScripts/Plant_Growth_Script.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class Plant_Growth_Script : MonoBehaviour
@@ -8,8 +9,20 @@ public class Plant_Growth_Script : MonoBehaviour
     [SerializeField] private GameObject[] PlantStage;
 
     private int nextStage = 0;
+    private bool dead = false;
+    private int dayPlanted;
+    private int plantLifeLength;
+    private int plantLife = 1;
+    private int dayCounter;
+    private int mistakeCounter = 0;
 
-    private bool seedPlanted = false;
+    public bool seedPlanted = false;
+    public int plant_ripe_length;
+    public int mistakesAllowed;
+    public bool waterTracker;
+
+    public Day_tracker day_tracker;
+    public garden_plot_state plot_state;
 
     // Start is called before the first frame update
     void Start()
@@ -20,52 +33,86 @@ public class Plant_Growth_Script : MonoBehaviour
             nextStage++;
         }
         nextStage = 1;
+        plantLifeLength = (PlantStage.Length - 1) + plant_ripe_length;
+
+        dayCounter = day_tracker.day;
+
+        Debug.Log("Plant life length = " + plantLifeLength);
 
     }
    
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("a"))
+        if (dead == false)
         {
-            PlantSeed();
-        }
-        if (Input.GetKeyDown("s"))
-        {
-            PlantGrowth();
+            waterTracker = plot_state.watered;
+            if (Input.GetKeyDown("a"))
+            {
+                PlantSeed();
+            }
+ 
+            if (dayCounter != day_tracker.day)
+            {
+                PlantGrowth();
+                dayCounter++;
+            }
         }
     }
 
     public void PlantSeed()
     {
-        if (seedPlanted == false)
+        //Plants seed by unhiding currently used plant
+        if ((seedPlanted == false) && (plot_state.tilled == true))
         {
             PlantStage[0].SetActive(true);
             seedPlanted = true;
-            Debug.Log("Seed planted");
+            dayPlanted = day_tracker.day;
+            Debug.Log("Seed planted on day " + dayPlanted);
         }
     }
 
     public void PlantGrowth()
     {
         // Set new plant stage and queue next plant stage
-        if (seedPlanted == true)
+        if ((seedPlanted == true))
         {
-            PlantStage[nextStage].SetActive(true);
-            if (nextStage == 0)
+            if (waterTracker == true)
             {
-                PlantStage[PlantStage.Length - 2].SetActive(false);
+                // Plant grows till maturity then dies after plantLife = plantLifeLength
+                if (plantLife != (plantLifeLength - 1))
+                {
+                    if (nextStage != PlantStage.Length - 1)
+                    {
+                        PlantStage[nextStage].SetActive(true);
+                        PlantStage[nextStage - 1].SetActive(false);
+                        nextStage++;
+                    }
+                    plantLife++;
+                }
+                else
+                {
+                    PlantStage[PlantStage.Length - 1].SetActive(true);
+                    PlantStage[PlantStage.Length - 2].SetActive(false);
+                    dead = true;
+                }
+
+                Debug.Log("plant stage" + nextStage);
+                waterTracker = false;
             }
             else
             {
-                PlantStage[nextStage - 1].SetActive(false);
+                // tracks days not watered and kills the plant if mistakes allowed is reached
+                if (mistakeCounter == mistakesAllowed)
+                {
+                    PlantStage[PlantStage.Length - 1].SetActive(true);
+                    PlantStage[nextStage - 1].SetActive(false);
+                    dead = true;
+                    Debug.Log("Plant is dead");
+                }
+                mistakeCounter++;
+
             }
-            nextStage++;
-            if (nextStage >= PlantStage.Length - 1)
-            {
-                nextStage = 0;
-            }
-            Debug.Log("plant stage" + nextStage);
         }
 
 
--- Assets/Scripts/RyanScripts/garden_plot_state.cs
+++ Assets/Scripts/RyanScripts/garden_plot_state.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class garden_plot_state : MonoBehaviour
 {
-    private bool watered = false;
-    private bool tilled = false;
+    public bool watered = false;
+    public bool tilled = false;
 
     [SerializeField] private MeshFilter PlotBase;
     [SerializeField] private Material PlotWet;
     [SerializeField] private Mesh PlotTilled;
 
+    private int dayCounter;
+
+    public Day_tracker day_tracker;
+    public Plant_Growth_Script plant_state;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        dayCounter = day_tracker.day;
     }
 
     // Update is called once per frame
@@ -27,6 +32,23 @@ public class garden_plot_state : MonoBehaviour
         if (Input.GetKeyDown("2") && (tilled == false))
         {
             tilling();
+        }
+        // sets watered state to false after a day has passed. Extra if statement is to ensure growth is triggered before watered state is set to false if a seed has been planted
+        if (plant_state.seedPlanted == true)
+        {
+            if ((dayCounter != day_tracker.day) && (plant_state.waterTracker == false))
+            {
+                watered = false;
+                dayCounter++;
+            }
+        }
+        else
+        {
+            if (dayCounter != day_tracker.day)
+            {
+                watered = false;
+                dayCounter++;
+            }
         }
     }
 
